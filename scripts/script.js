32ee151c78f1cf8bf2f152a268e9e3df44324372
robotscissors <cjbazin@gmail.com>
2018-09-13 17:03:27 -0700

    more javascript work

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
-	
+	var windowHeight = $(window).height(); // get height of window
 	var navOffset = $("nav").offset().top;
-	$("status").html(navOffset);
+	
+	// need to update the height if the window has changed sizes
+	
+	$(window).resize(function() {
+		windowHeight = $(window).height();
+		$(".status").html(windowHeight);
+	});
+	
 	$(window).scroll(function() {
-		
 		var scrollPosition = $(window).scrollTop();
-
-		$(".status").html(scrollPosition);
+		$(".status").html(windowHeight);
 		if (scrollPosition >= navOffset) {
 			$("nav").addClass("fixed");
-			alert("fixed");
+			//alert("fixed");
 		} else {
-			$("nav").removeClass("fixed");
+			$("nav").removeClass("fixed");			
 		}
 	});
 	
