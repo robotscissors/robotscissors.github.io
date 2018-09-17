3c8729a4e62c1cce006888f1ba7ad8c4a8bac59c
Christopher Bazin <christopherbazin@Christophers-MacBook.local>
2018-09-17 15:30:23 -0700

    still working

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,42 @@
 $(document).ready(function(){
 	var windowHeight = $(window).height(); // get height of window
 	var navOffset = $("nav").offset().top;
+	var scrollPosition = $(window).scrollTop();
 	
 	// need to update the height if the window has changed sizes
-	
+	$(".status").html(600-windowHeight-scrollPosition);
 	$(window).resize(function() {
 		windowHeight = $(window).height();
-		$(".status").html(windowHeight);
 	});
 	
+	//on load ---------------------------------------->
+	// window is bigger than 600px height
 	if (windowHeight>600) {
-		// add class relative
+		// window is big enough
+		// bar should be at bottom of window
 		$("nav").addClass("relative");
+		$("nav").removeClass("fixed-position-bottom");
 	}
-	
+	// window is less than 600px high
+	if (windowHeight <= 600) {
+		//stick it on the bottom
+		$("nav").addClass("fixed-position-bottom");
+		$("nav").removeClass("relative");
+	}
+	//on scroll ---------------------------------------->	
 	$(window).scroll(function() {
-		var scrollPosition = $(window).scrollTop();
-		if (windowHeight>600) {
-		// add class relative
-		$("nav").addClass("relative");
-	}
-		$(".status").html(windowHeight);
+  	var scrollPosition = $(window).scrollTop();
+		var windowHeight = $(window).height(); // get height of window
+		var navOffset = $("nav").offset().top	
 		if (scrollPosition >= navOffset) {
 			$("nav").addClass("fixed");
-			//alert("fixed");
+			$("nav").removeClass("fixed-position-bottom")
+			$("nav").removeClass("relative");
+			$(".status").html("freeze");
 		} else {
-			$("nav").removeClass("fixed");			
+			
+			$(".status").html(600-windowHeight-scrollPosition);
 		}
+		
 	});
-	
 });
